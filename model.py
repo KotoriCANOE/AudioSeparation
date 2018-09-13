696e1341f1482c9ca540d3b2a2ae0e12eacdc101
KotoriCANOE <sanma.kote@gmail.com>
2018-09-13 19:37:42 +0800

    Initial commit

---
+++ model.py
@@ -0,0 +1,129 @@
+import tensorflow as tf
+from network import Generator
+
+DATA_FORMAT = 'NCHW'
+
+class SpeakerTransfer:
+    def __init__(self, config):
+        # format parameters
+        self.dtype = tf.float32
+        self.data_format = DATA_FORMAT
+        self.in_channels = 2
+        self.out_channels = 2
+        # collections
+        self.train_sums = []
+        self.loss_sums = []
+        # copy all the properties from config object
+        self.config = config
+        self.__dict__.update(config.__dict__)
+        # internal parameters
+        self.input_shape = [None, None, None, None]
+        self.input_shape[-3 if self.data_format == 'NCHW' else -1] = self.in_channels
+        self.output_shape = self.input_shape
+
+    def build_model(self, inputs=None):
+        # inputs
+        if inputs is None:
+            self.inputs = tf.placeholder(self.dtype, self.input_shape, name='Input')
+        else:
+            self.inputs = tf.identity(inputs, name='Input')
+            self.inputs.set_shape(self.input_shape)
+        # forward pass
+        self.generator = Generator('Generator', self.config)
+        self.outputs = self.generator(self.inputs, reuse=None)
+        # outputs
+        self.outputs = tf.identity(self.outputs, name='Output')
+        # all the saver variables
+        self.svars = self.generator.svars
+        # all the restore variables
+        self.rvars = self.generator.rvars
+        # return outputs
+        return self.outputs
+
+    def build_train(self, inputs=None, labels=None):
+        # style reference
+        if labels is None:
+            self.labels = tf.placeholder(self.dtype, self.output_shape, name='Label')
+        else:
+            self.labels = tf.identity(labels, name='Label')
+            self.labels.set_shape(self.output_shape)
+        # build model
+        self.build_model(inputs)
+        # build loss
+        self.build_g_loss(self.outputs, self.labels)
+
+    def build_g_loss(self, outputs, labels):
+        self.g_log_losses = []
+        update_ops = []
+        loss_key = 'GeneratorLoss'
+        with tf.variable_scope(loss_key):
+            l1_loss = tf.losses.absolute_difference(outputs, labels)
+            update_ops.append(self.loss_summary('l1_loss', l1_loss, self.g_log_losses))
+            # total loss
+            losses = tf.losses.get_losses(loss_key)
+            main_loss = tf.add_n(losses, 'main_loss')
+            # regularization loss
+            reg_losses = tf.losses.get_regularization_losses('Generator')
+            reg_loss = tf.add_n(reg_losses)
+            update_ops.append(self.loss_summary('reg_loss', reg_loss))
+            # final loss
+            self.g_loss = main_loss + reg_loss
+            update_ops.append(self.loss_summary('loss', self.g_loss))
+            # accumulate operator
+            with tf.control_dependencies(update_ops):
+                self.g_losses_acc = tf.no_op('accumulator')
+
+    def train(self, global_step):
+        model = self.generator
+        # dependencies to be updated
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS, 'Generator')
+        # learning rate
+        lr = tf.train.cosine_decay_restarts(1e-3,
+            global_step, 1000, t_mul=2.0, m_mul=1.0, alpha=1e-1)
+        lr = tf.train.exponential_decay(lr, global_step, 1000, 0.99)
+        self.train_sums.append(tf.summary.scalar('Generator/LR', lr))
+        # optimizer
+        opt = tf.contrib.opt.NadamOptimizer(lr)
+        with tf.control_dependencies(update_ops):
+            grads_vars = opt.compute_gradients(self.g_loss, model.tvars)
+            update_ops = [opt.apply_gradients(grads_vars, global_step)]
+        # histogram for gradients and variables
+        for grad, var in grads_vars:
+            self.train_sums.append(tf.summary.histogram(var.op.name + '/grad', grad))
+            self.train_sums.append(tf.summary.histogram(var.op.name, var))
+        # save moving average of trainalbe variables
+        model.apply_ema(update_ops)
+        # all the saver variables
+        self.svars = self.generator.svars
+        # return optimizing op
+        with tf.control_dependencies(update_ops):
+            train_op = tf.no_op('train')
+        return train_op
+
+    def loss_summary(self, name, loss, collection=None):
+        with tf.variable_scope('LossSummary/' + name):
+            # internal variables
+            loss_sum = tf.get_variable('sum', (), tf.float32, tf.initializers.zeros(tf.float32))
+            loss_count = tf.get_variable('count', (), tf.float32, tf.initializers.zeros(tf.float32))
+            # accumulate to sum and count
+            acc_sum = loss_sum.assign_add(loss, True)
+            acc_count = loss_count.assign_add(1.0, True)
+            # calculate mean
+            loss_mean = tf.divide(loss_sum, loss_count, 'mean')
+            if collection is not None:
+                collection.append(loss_mean)
+            # reset sum and count
+            with tf.control_dependencies([loss_mean]):
+                clear_sum = loss_sum.assign(0.0, True)
+                clear_count = loss_count.assign(0.0, True)
+            # log summary
+            with tf.control_dependencies([clear_sum, clear_count]):
+                self.loss_sums.append(tf.summary.scalar('value', loss_mean))
+            # return after updating sum and count
+            with tf.control_dependencies([acc_sum, acc_count]):
+                return tf.identity(loss, 'loss')
+
+    def get_summaries(self):
+        train_summary = tf.summary.merge(self.train_sums) if self.train_sums else None
+        loss_summary = tf.summary.merge(self.loss_sums) if self.loss_sums else None
+        return train_summary, loss_summary
---
+++ network.py
@@ -0,0 +1,220 @@
+import tensorflow as tf
+import tensorflow.contrib.layers as slim
+import layers
+
+ACTIVATION = layers.Swish
+DATA_FORMAT = 'NCHW'
+
+class GeneratorConfig:
+    def __init__(self):
+        # format parameters
+        self.dtype = tf.float32
+        self.data_format = DATA_FORMAT
+        self.in_channels = 2
+        self.out_channels = 2
+        # model parameters
+        self.activation = ACTIVATION
+        self.normalization = 'Instance'
+        # train parameters
+        self.random_seed = 0
+        self.var_ema = 0.999
+        self.weight_decay = 1e-6
+
+class Generator(GeneratorConfig):
+    def __init__(self, name='Generator', config=None):
+        super().__init__()
+        self.name = name
+        # copy all the properties from config object
+        if config is not None:
+            self.__dict__.update(config.__dict__)
+        # create a moving average object for trainable variables
+        if self.var_ema > 0:
+            self.ema = tf.train.ExponentialMovingAverage(self.var_ema)
+
+    def ResBlock(self, last, channels, kernel=[1, 3], stride=[1, 1], biases=True, format=DATA_FORMAT,
+        dilate=1, activation=ACTIVATION, normalizer=None,
+        regularizer=None, collections=None):
+        biases = tf.initializers.zeros(self.dtype) if biases else None
+        initializer = tf.initializers.variance_scaling(
+            1.0, 'fan_in', 'normal', self.random_seed, self.dtype)
+        skip = last
+        if normalizer: last = normalizer(last)
+        if activation: last = activation(last)
+        last = slim.conv2d(last, channels, kernel, stride, 'SAME', format,
+            dilate, activation, normalizer, None, initializer, regularizer, biases,
+            variables_collections=collections)
+        last = slim.conv2d(last, channels, kernel, stride, 'SAME', format,
+            dilate, None, None, None, initializer, regularizer, biases,
+            variables_collections=collections)
+        # skip connection
+        last = layers.SEUnit(last, channels, format, collections)
+        last += skip
+        return last
+
+    def EBlock(self, last, channels, resblocks=1,
+        kernel=[1, 4], stride=[1, 2], format=DATA_FORMAT,
+        activation=ACTIVATION, normalizer=None, regularizer=None, collections=None):
+        initializer = tf.initializers.variance_scaling(
+            1.0, 'fan_in', 'normal', self.random_seed, self.dtype)
+        # pre-activation
+        if activation: last = activation(last)
+        # convolution
+        last = slim.conv2d(last, channels, kernel, stride, 'SAME', format,
+            1, None, None, weights_initializer=initializer,
+            weights_regularizer=regularizer, variables_collections=collections)
+        # ResBlocks
+        for i in range(resblocks):
+            with tf.variable_scope('ResBlock_{}'.format(i)):
+                last = self.ResBlock(last, channels, format=format,
+                    activation=activation, normalizer=normalizer,
+                    regularizer=regularizer, collections=collections)
+        # squeeze and excitation
+        last = layers.SEUnit(last, channels, format, collections)
+        return last
+
+    def DBlock(self, last, channels, resblocks=1,
+        kernel=[1, 3], stride=[1, 2], format=DATA_FORMAT,
+        activation=ACTIVATION, normalizer=None, regularizer=None, collections=None):
+        initializer = tf.initializers.variance_scaling(
+            1.0, 'fan_in', 'normal', self.random_seed, self.dtype)
+        # shape = last.shape.as_list()
+        # in_channels = shape[-3 if format == 'NCHW' else -1]
+        # pre-activation
+        if activation: last = activation(last)
+        # upsample
+        with tf.variable_scope('Upsample'):
+            upsize = tf.shape(last)
+            upsize = upsize[-2:] if format == 'NCHW' else upsize[-3:-1]
+            upsize = upsize * stride[0:2]
+            if format == 'NCHW':
+                last = tf.transpose(last, (0, 2, 3, 1))
+            last = tf.image.resize_nearest_neighbor(last, upsize)
+            if format == 'NCHW':
+                last = tf.transpose(last, (0, 3, 1, 2))
+        # convolution
+        last = slim.conv2d(last, channels, kernel, [1, 1], 'SAME', format,
+            1, None, None, weights_initializer=initializer,
+            weights_regularizer=regularizer, variables_collections=collections)
+        # ResBlocks
+        for i in range(resblocks):
+            with tf.variable_scope('ResBlock_{}'.format(i)):
+                last = self.ResBlock(last, channels, format=format,
+                    activation=activation, normalizer=normalizer,
+                    regularizer=regularizer, collections=collections)
+        # squeeze and excitation
+        last = layers.SEUnit(last, channels, format, collections)
+        return last
+
+    def __call__(self, last, reuse=None):
+        format = self.data_format
+        kernel1 = [1, 4]
+        stride1 = [1, 2]
+        kernel2 = [1, 3]
+        stride2 = [1, 2]
+        # function objects
+        activation = self.activation
+        if self.normalization == 'Batch':
+            normalizer = lambda x: slim.batch_norm(x, 0.999, center=True, scale=True,
+                is_training=self.training, data_format=format, renorm=False)
+        elif self.normalization == 'Instance':
+            normalizer = lambda x: slim.instance_norm(x, center=True, scale=True, data_format=format)
+        else:
+            normalizer = None
+        regularizer = slim.l2_regularizer(self.weight_decay) if self.weight_decay else None
+        # model scope
+        with tf.variable_scope(self.name, reuse=reuse):
+            # states
+            self.training = tf.Variable(False, trainable=False, name='training',
+                collections=[tf.GraphKeys.GLOBAL_VARIABLES, tf.GraphKeys.MODEL_VARIABLES])
+            skips = []
+            # encoder
+            with tf.variable_scope('InBlock'):
+                last = self.EBlock(last, 32, 0, [1, 8], [1, 2],
+                    format, None, None, regularizer)
+            with tf.variable_scope('EBlock_1'):
+                skips.append(last)
+                last = self.EBlock(last, 48, 0, kernel1, stride1,
+                    format, activation, normalizer, regularizer)
+            with tf.variable_scope('EBlock_2'):
+                skips.append(last)
+                last = self.EBlock(last, 64, 1, kernel1, stride1,
+                    format, activation, normalizer, regularizer)
+            with tf.variable_scope('EBlock_3'):
+                skips.append(last)
+                last = self.EBlock(last, 96, 1, kernel1, stride1,
+                    format, activation, normalizer, regularizer)
+            with tf.variable_scope('EBlock_4'):
+                skips.append(last)
+                last = self.EBlock(last, 128, 2, kernel1, stride1,
+                    format, activation, normalizer, regularizer)
+            with tf.variable_scope('EBlock_5'):
+                skips.append(last)
+                last = self.EBlock(last, 160, 2, kernel1, stride1,
+                    format, activation, normalizer, regularizer)
+            with tf.variable_scope('EBlock_6'):
+                skips.append(last)
+                last = self.EBlock(last, 192, 2, kernel1, stride1,
+                    format, activation, normalizer, regularizer)
+            with tf.variable_scope('EBlock_7'):
+                skips.append(last)
+                last = self.EBlock(last, 224, 3, kernel1, stride1,
+                    format, activation, normalizer, regularizer)
+            with tf.variable_scope('EBlock_8'):
+                skips.append(last)
+                last = self.EBlock(last, 256, 3, kernel1, stride1,
+                    format, activation, normalizer, regularizer)
+            # decoder
+            with tf.variable_scope('DBlock_8'):
+                last = self.DBlock(last, 224, 3, kernel2, stride2,
+                    format, activation, normalizer, regularizer)
+                last = tf.concat([last, skips.pop()], -3)
+            with tf.variable_scope('DBlock_7'):
+                last = self.DBlock(last, 192, 2, kernel2, stride2,
+                    format, activation, normalizer, regularizer)
+                last = tf.concat([last, skips.pop()], -3)
+            with tf.variable_scope('DBlock_6'):
+                last = self.DBlock(last, 160, 2, kernel2, stride2,
+                    format, activation, normalizer, regularizer)
+                last = tf.concat([last, skips.pop()], -3)
+            with tf.variable_scope('DBlock_5'):
+                last = self.DBlock(last, 128, 2, kernel2, stride2,
+                    format, activation, normalizer, regularizer)
+                last = tf.concat([last, skips.pop()], -3)
+            with tf.variable_scope('DBlock_4'):
+                last = self.DBlock(last, 96, 1, kernel2, stride2,
+                    format, activation, normalizer, regularizer)
+                last = tf.concat([last, skips.pop()], -3)
+            with tf.variable_scope('DBlock_3'):
+                last = self.DBlock(last, 64, 1, kernel2, stride2,
+                    format, activation, normalizer, regularizer)
+                last = tf.concat([last, skips.pop()], -3)
+            with tf.variable_scope('DBlock_2'):
+                last = self.DBlock(last, 48, 0, kernel2, stride2,
+                    format, activation, normalizer, regularizer)
+                last = tf.concat([last, skips.pop()], -3)
+            with tf.variable_scope('DBlock_1'):
+                last = self.DBlock(last, 32, 0, kernel2, stride2,
+                    format, activation, normalizer, regularizer)
+                last = tf.concat([last, skips.pop()], -3)
+            with tf.variable_scope('OutBlock'):
+                last = self.DBlock(last, self.in_channels, 0, kernel2, stride2,
+                    format, activation, normalizer, regularizer)
+        # trainable/model/save/restore variables
+        self.tvars = tf.trainable_variables(self.name)
+        self.mvars = tf.model_variables(self.name)
+        self.mvars = [i for i in self.mvars if i not in self.tvars]
+        self.svars = list(set(self.tvars + self.mvars))
+        self.rvars = self.svars.copy()
+        # restore moving average of trainable variables
+        if self.var_ema > 0:
+            self.rvars = {**{self.ema.average_name(var): var for var in self.tvars},
+                **{var.op.name: var for var in self.mvars}}
+        return last
+
+    def apply_ema(self, update_ops=[]):
+        if not self.var_ema:
+            return
+        with tf.variable_scope(self.name + 'EMA'):
+            with tf.control_dependencies(update_ops):
+                update_ops = [self.ema.apply(self.tvars)]
+            self.svars = [self.ema.average(var) for var in self.tvars] + self.mvars
